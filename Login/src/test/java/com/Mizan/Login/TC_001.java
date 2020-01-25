2f00386baed44b7a91110d14ab8b80627179cf33
naima <naima@Rahman-PC.fios-router.home>
2020-01-25 15:34:37 -0500

    First Commit for repo

---
+++ Login/src/test/java/com/Mizan/Login/TC_001.java
@@ -0,0 +1,24 @@
+package com.Mizan.Login;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_001 {
+	
+	@Test
+
+	public void testcase1()
+	{
+		
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.navigate().to("http://www.facebook.com");
+		driver.manage().window().maximize();
+		driver.findElementById("email").sendKeys("Hello");
+		driver.findElementById("pass").sendKeys("Hello");
+		driver.quit();
+		
+		
+	}
+
+}
---
+++ Login/src/test/java/com/Mizan/Login/TC_003.java
@@ -0,0 +1,21 @@
+package com.Mizan.Login;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_003 {
+	@Test
+
+	public void testcase3() {
+
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.navigate().to("http://www.facebook.com");
+		driver.manage().window().maximize();
+		driver.findElementById("email").sendKeys("Hello");
+		driver.findElementById("pass").sendKeys("Hello");
+		driver.quit();
+
+	}
+
+}
